335f33dca98d2ca8eb4164bacc5e7ea23be3de8e
Jakub Nowacki <jnowacki@kamsoft.pl>
2020-11-02 22:39:17 +0100

    database v1.6 test

--- database/Insert_data.sql
+++ database/Insert_data.sql
@@ -1,2 +1,5 @@
 INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
-VALUES ('JAKUB','NOWACKI',to_date('1999-02-01','yyyy-mm-dd'),'Leszno','93020100012',1,'CJU938487','M',null,'DASZYŃSKIEGO','24','5','A1','RENATA','WLADYSLAW','509677027',null);
+VALUES ('JAKUB','NOWACKI',to_date('1993-02-01','yyyy-mm-dd'),'LESZNO','93020100012',1,'CJU938487','M',null,'DASZYŃSKIEGO','24','5','A1','RENATA','WLADYSLAW','509677027',null);
+\
+INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
+VALUES ('PAULINA','KWIAT',to_date('1997-12-05','yyyy-mm-dd'),'LESZNO','97120510124',1,'CLD542156','K',null,'POLNA','13A',NULL,'A4','KRYSTYNA','ADAM','605486689',null);
