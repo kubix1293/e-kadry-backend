701eca55ec1e1866add602f1bfc46aecf52e99c2
Jakub Nowacki <kubix1293@gmail.com>
2020-11-03 23:12:58 +0100

    database v1.7

--- database/Insert_data.sql
+++ database/Insert_data.sql
@@ -3,3 +3,12 @@ VALUES ('JAKUB','NOWACKI',to_date('1993-02-01','yyyy-mm-dd'),'LESZNO','930201000
 \
 INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
 VALUES ('PAULINA','KWIAT',to_date('1997-12-05','yyyy-mm-dd'),'LESZNO','97120510124',1,'CLD542156','K',null,'POLNA','13A',NULL,'A4','KRYSTYNA','ADAM','605486689',null);
+\
+INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
+VALUES ('ALEKSANDER','NOWAK',to_date('1989-09-21','yyyy-mm-dd'),'LASOCICE','89092100335',1,'CDP945427','M',null,'KOSYNIERÓW','5',null,'A2','MARLENA','ADAM',null,null);
+\
+INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
+VALUES ('MARLENA','KOWALSKA',to_date('1976-07-12','yyyy-mm-dd'),'LESZNO','76071225228',1,'CDU765412','K',null,'NIEPODLEGŁOŚCI','34','12','A3','AGATA','BRONISŁAW',null,null);
+\
+INSERT INTO pracownicy (imie,nazwisko,dtur,misc_uro,pesel,dok_typ,nr_dok,plec,id_misc,ulica,nr_dom,nr_lok,nr_akt,imie_mat,imie_ojc,tele,id_oper)
+VALUES ('BARTOSZ','MOLIK',to_date('1963-10-30','yyyy-mm-dd'),'WSCHOWA','63103068911',1,'OPK887654','M',null,'POLNA','2',null,'A5','BARBARA','ROMAN',null,null);
